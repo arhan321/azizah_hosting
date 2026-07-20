962f187ad84bbe00026be738ab828fe9efd48f5e
arhan321 <arhanmcz@gmail.com>
2026-07-20 17:42:26 +0000

    update

--- src/config/app.php
+++ src/config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://azizah.djncloud.my.id'),
+    'url' => env('APP_URL', 'https://aqlammuralkaligrafi.my.id'),
 
     /*
     |--------------------------------------------------------------------------
